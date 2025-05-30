6231ceeb99a692bc02874fc25b8f65314ede1461
Nikhil-Jassal <200566092@student.georgianc.on.ca>
2025-05-29 23:35:57 -0400

    added code to ask user to enter teir choice

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -3,3 +3,10 @@ const prompt = require('prompt');
 
 //start the prompt
 prompt.start();
+
+// Step 3: Ask the user to enter their choice (ROCK, PAPER, or SCISSORS)
+prompt.get(['userChoice'], function (err, result) {
+    if (err) {
+      console.log('There was an error getting input.');
+      return;
+    }
