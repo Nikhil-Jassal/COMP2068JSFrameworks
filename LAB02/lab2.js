b7a853dd2d7aadf1256c9b8502489e81b6c8a9db
Nikhil-Jassal <200566092@student.georgianc.on.ca>
2025-05-29 23:41:42 -0400

    convert user input to upper case and generate a random number

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -4,9 +4,20 @@ const prompt = require('prompt');
 //start the prompt
 prompt.start();
 
-// Step 3: Ask the user to enter their choice (ROCK, PAPER, or SCISSORS)
+//ask the user to enter their choice (ROCK, PAPER, or SCISSORS)
 prompt.get(['userChoice'], function (err, result) {
     if (err) {
       console.log('There was an error getting input.');
       return;
     }
+
+      //convert user input to uppercase
+  const userSelection = result.userChoice.toUpperCase();
+
+  //generate a random number between 0 and 1
+  const randomNumber = Math.random();
+  let computerSelection = '';
+
+
+
+});
